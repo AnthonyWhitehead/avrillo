f24847a3addfec9691039c037bf9f0a3913aa996
Anthony Whitehead <antdwhitehead@gmail.com>
2024-04-27 11:17:40 +0100

    test token creation

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\TokenController;
 use App\Http\Middleware\ValidateToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('token', [TokenController::class, 'create'])->name('token.create');
+Route::name('api.')->group(function () {
+    Route::get('token', [TokenController::class, 'create'])->name('token.create');
 
-Route::group(['middleware' => ValidateToken::class], function () {
-    Route::get('kayne-quotes', function () {
-        return response()->json(['message' => 'I am a god']);
-    })->name('kayne');
+    Route::group(['middleware' => ValidateToken::class], function () {
+        Route::get('kayne-quotes', function () {
+            return response()->json(['message' => 'I am a god']);
+        })->name('kayne');
+    });
 });
---
+++ tests/Feature/Token/TokenTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Token;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class TokenTest extends TestCase
+{
+    /**
+     * Test token creation
+     */
+    public function test_create_token(): void
+    {
+        $response = $this->get(route('api.token.create'));
+
+        $response->assertCreated();
+
+        $response->assertJson(fn(AssertableJson $json) => $json->has('data')
+            ->has('data.token')
+            ->has('data.expires_at')
+            ->where('data.expires_at', Carbon::now()->addMinutes((int)config('tokens.expiry'))->toDateTimeString())
+        );
+    }
+}
